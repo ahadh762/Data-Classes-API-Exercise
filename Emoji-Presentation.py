32b4a1d98460af2ec3377035c1a690a46efbfdf9
“Ahad <ahussain@dev-10.com>
2022-04-14 12:28:50 -0400

    initial commit

---
+++ Emoji-Presentation.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+
+@dataclass
+class animals_and_nature:
+    name: str
+    group: str
+    htmlCode: str
+    unicode: str
+
+
+@dataclass
+class food_and_drink:
+    name: str
+    group: str
+    htmlCode: str
+    unicode: str
+
+
+@dataclass
+class travel_and_places:
+    name: str
+    group: str
+    htmlCode: str
+    unicode: str
+
+
+@dataclass
+class activities:
+    name: str
+    group: str
+    htmlCode: str
+    unicode: str
+
+
+@dataclass
+class objects:
+    name: str
+    group: str
+    htmlCode: str
+    unicode: str
